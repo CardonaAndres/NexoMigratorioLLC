5f8608e334c9ff214546a6fb8ccf627062a6c8ac
Andrés Cardona <135064128+Totoy777@users.noreply.github.com>
2025-01-29 13:04:54 -0500

    Actualizar sitemap y URL base; ajustar dimensiones del logo en función del tamaño de la ventana

--- src/components/navbar/LogoCommponent.jsx
+++ src/components/navbar/LogoCommponent.jsx
@@ -1,16 +1,39 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/imgs/LogoNexoMigratorio.webp';
 
 export const LogoCommponent = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const getImageDimensions = () => {
+    if (windowWidth < 660) { // móvil
+      return 'h-9 w-auto';
+    } else if (windowWidth < 1024) { // tablet
+      return 'h-16 w-auto';
+    }
+    return 'h-20 w-auto'; // desktop
+  };
+
   return (
-    <div className="flex items-center">
-        <div className="flex-shrink-0 flex items-center">
-        <img className="h-full w-full" src={Logo} alt="Logo" loading='lazy'/>
+    <Link to="/" className="flex items-center">
+      <div className="flex-shrink-0 flex items-center">
+        <img className={`${getImageDimensions()} object-contain`} src={Logo} 
+          alt="Logo Nexo Migratorio" loading="lazy" decoding="async"draggable="false"
+        />
         <div className="ml-3">
-            <h1 className="text-xl font-bold text-gray-800 hidden">
-                Nexo Migratorio LCC
-            </h1>
+          <h1 className="text-xl font-bold text-gray-800 hidden sm:block">
+            Nexo Migratorio LCC
+          </h1>
         </div>
-        </div>
-    </div>
+      </div>
+    </Link>
   )
 }
