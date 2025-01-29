ef3d656be460043ce66cd5f9e16227e25f5cd171
Andrés Cardona <135064128+CardonaAndres@users.noreply.github.com>
2025-01-29 12:40:40 -0500

    Cambio de URL al actualizar el dominio

--- src/configs/config.js
+++ src/configs/config.js
@@ -7,4 +7,4 @@ export const router = {
     location : '/Ubicacion'
 }
 
-export const BASE_URL = "https://nexo-migratorio-llc.vercel.app";
+export const BASE_URL = "https://www.nexomigratorio.com";
